cd37e52beb1a7ee5a79afe3887852e410789c848
Roger Erill <rerill@ikomobi.com>
2017-12-02 19:43:36 +0100

    Initial commit

---
+++ mobile/src/main/java/com/erill/simplebitcoinchecker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.erill.simplebitcoinchecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
